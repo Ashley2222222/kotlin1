fc64e07f21441992e4ab1874b61cb3192d3f8413
liangxy <tot.123@163.com>
2022-01-14 18:34:33 +0800

    databinding
    databinding

---
+++ AboutMe/app/src/main/java/com/ashleyLeung/MyName.kt
@@ -0,0 +1,10 @@
+package com.ashleyLeung
+
+/**
+ * @author lxy
+ * @name
+ * @class describe
+ * @date 2022-01-14
+ * Copyright (c)  Guangzhou GCI Science & Technology Co., Ltd.All rights reserved
+ */
+data class MyName (var name:String= "",var nickname:String="")
--- AboutMe/app/src/main/java/com/ashleyLeung/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/ashleyLeung/aboutme/MainActivity.kt
@@ -8,38 +8,56 @@ import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
+import com.ashleyLeung.MyName
 import com.ashleyLeung.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private var myName : MyName = MyName("Ashley Leung222")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        setContentView(R.layout.activity_main)
+        //        setContentView(R.layout.activity_main)//databinding的时候这句一定要删掉
 //        findViewById<Button>(R.id.button).setOnClickListener {
 //            addNickName(it)
 //        }
+//        val starImage = findViewById<ImageView>(R.id.star_image);
+
+    /*viewbinding
+
+     binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         binding.starImage.visibility = View.GONE
         binding.button.setOnClickListener {
             Log.d("name", "aaaa");
             addNickName(it)
+        }*/
+        //databinding
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.myName = myName
+
+
+        binding.button.setOnClickListener {
+
+            addNickName(it)
+            Log.d("name", myName.name+","+myName.nickname);
         }
     }
 
     private fun addNickName(view: View) {
-        Log.d("name", "bbbb");
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nickNameTextView = findViewById<TextView>(R.id.nickname_text)
+//        Log.d("name", "bbbb");
+//        val editText = findViewById<EditText>(R.id.nickname_edit)
+//        val nickNameTextView = findViewById<TextView>(R.id.nickname_text)
 
 //        nickNameTextView.text = editText.text
 //        editText.visibility = View.GONE
 //        view.visibility = View.GONE
 //        nickNameTextView.visibility = View.VISIBLE
 
-       binding.apply {
+     /*viewbinding
+     binding.apply {
            Log.d("name", "cccc");
             binding.nicknameText.text = binding.nicknameEdit.text
             invalidateAll()
@@ -47,6 +65,13 @@ class MainActivity : AppCompatActivity() {
             view.visibility = View.GONE
             binding.nicknameText.visibility = View.VISIBLE
            Log.d("name", "dddd");
+        }*/
+
+        binding.apply {
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            button.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
         }
 
 
